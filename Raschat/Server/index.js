b247815b99aa575baaa3227fe6764f626a5bb6fe
gmarck04 <gmarck04@gmail.com>
2022-04-27 18:59:03 +0200

    Socket

---
+++ Raschat/Server/index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var { Server } = require("socket.io");
+var io = new Server(server);
+
+const port = 8080;
+const hostname = 'localhost'
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('message', 'Sei connesso amico!');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+  });
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
